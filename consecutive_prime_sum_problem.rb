075c8f5d9536aaa3254537672faca49c8c45aeed
Shankar <shankar.yuvasoft15@gmail.com>
2018-03-23 18:38:12 +0530

    Improvement#2

--- consecutive_prime_sum_problem.rb
+++ consecutive_prime_sum_problem.rb
@@ -2,28 +2,28 @@ require 'active_support'
 require 'active_support/core_ext'
 class Fixnum (require 'prime'); end
 def find_sequence
-  @final_prime_arr.clear
+  @ranged_primes = Prime.take_while {|p| p < 500000 }.sort.reverse
+  @final_prime_arr = []
+  @final_prime_hash = [0,[0]]
   @sum_range = 0
-  while @ranged_primes.present? do
-    ele = @ranged_primes.shift
+  @ranged_primes.each do |ele|
     @sum_range += ele
     @final_prime_arr << ele
     if @sum_range>1000000
       @sum_range = 0
-      @final_prime_arr.clear
-    elsif @sum_range.prime?
-      @final_prime_hash.clear
-      @final_prime_hash[@sum_range] = @final_prime_arr
+      @final_prime_arr = []
+    elsif @sum_range.prime? && @sum_range<1000000
+      if @final_prime_hash.last.size<@final_prime_arr.size
+        @final_prime_hash = []
+        @final_prime_hash = [@final_prime_arr.size, @final_prime_arr]
+      end
     end
   end
-  return @final_prime_hash[@final_prime_hash.keys.max]
+  @final_prime_hash
 end
 def get_most_large_prime
-  @ranged_primes = Prime.take_while {|p| p < 500000 }.sort.reverse
-  @final_prime_arr = []
-  @final_prime_hash = {}
-  find_sequence
-  puts "Prime Number => #{@final_prime_hash.keys.last}"
-  puts "Consecutive Prime Numbers => #{@final_prime_hash.values.last.reverse}"
+  @final_scequence = find_sequence
+  puts "Prime Number => #{@final_scequence.last.sum}"
+  puts "Consecutive Prime Numbers => #{@final_scequence.last.reverse}"
 end
 get_most_large_prime
